f5b66f94632d835c546db0d473f8c4228a1dfce0
M Taha Makhdoom <116482118+MTahaCode@users.noreply.github.com>
2024-03-20 06:35:21 +0500

    Time: 17 ms (18.74%), Space: 22.6 MB (5.13%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval)
+    {
+        vector<vector<int>> FinalIntervals;
+
+        vector<int> temp = newInterval;
+
+        for (vector<int> curr : intervals)
+        {
+            if (are_overlapping(curr, temp)) //overlap is in same order
+            {
+                // combine the curr and temp and replace it with temp
+                temp = combineInterval(curr, temp);
+            }
+            else // no overlap
+            {
+                if (curr[0] < temp[0]) //current is smaller than the temp
+                {
+                    //add current to final
+                    FinalIntervals.push_back(curr);
+                }
+                else
+                {
+                    //add temp to final and replace temp with current
+                    FinalIntervals.push_back(temp);
+                    temp = curr;
+                }
+            }
+        }
+        FinalIntervals.push_back(temp);
+
+        return FinalIntervals;
+    }
+    
+    vector<int> combineInterval(vector<int>& interval1, vector<int>& interval2)
+    {
+        return { min(interval1[0], interval2[0]), max(interval1[1], interval2[1]) };
+    }
+
+    bool are_overlapping(vector<int> interval1, vector<int> interval2)
+    {
+        if (interval1[0] < interval2[0])//interval are in same order
+        {
+            if (interval1[1] - interval2[0] < 0)// no overlap
+            {
+                return 0;
+            }
+            else// is overlap
+            {
+                return 1;
+            }
+        }
+        else if (interval1[0] > interval2[0]) //interval in reverse order
+        {
+            if (interval2[1] - interval1[0] < 0)// no overlap
+            {
+                return 0;
+            }
+            else// is overlap
+            {
+                return 1;
+            }
+        }
+        return 1;//overlap anyway
+    }
+};
+
+
+
