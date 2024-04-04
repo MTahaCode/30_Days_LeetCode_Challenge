e66d4e4379a570b76d6af14019ec33e88ae84ce7
M Taha Makhdoom <116482118+MTahaCode@users.noreply.github.com>
2024-04-04 08:17:23 +0500

    Time: 5 ms (34.03%), Space: 17.2 MB (72.73%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+        if (!root)
+        {
+            return true;
+        }
+        
+        return isRecurrSymmetric(root->left, root->right);
+    }
+    
+    bool isRecurrSymmetric(TreeNode* left, TreeNode* right)
+    {
+        if (!left && !right)
+        {
+            return true;
+        }
+        else if (!left || !right)
+        {
+            return false;
+        }
+        
+        return (left->val == right->val) 
+            && isRecurrSymmetric(left->left, right->right) 
+            && isRecurrSymmetric(left->right, right->left);
+    }
+};
