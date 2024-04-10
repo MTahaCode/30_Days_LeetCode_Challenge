1a570b8cdc81488d1b6e83102fb7bc549c48ec74
M Taha Makhdoom <116482118+MTahaCode@users.noreply.github.com>
2024-04-10 16:08:43 +0500

    Time: 26 ms (31.83%), Space: 75.8 MB (92.70%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,27 +11,33 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return helperAdd(l1, l2, 0);
-    }
+        
+        int carry = 0;
+        ListNode* SumHead = nullptr;
 
-    ListNode* helperAdd(ListNode* l1, ListNode* l2, int carry) {
-        if (!l1 && !l2)  {
-            if (!carry) return nullptr;
-            else return new ListNode(carry);
-        }
+        ListNode* curr = nullptr;
+        while (l1 || l2) {
+            int currVal = carry + ((l1) ? l1->val : 0) + ((l2) ? l2->val : 0);
 
-        int currVal = carry + ((l1) ? l1->val : 0) + ((l2) ? l2->val : 0);
-        ListNode* currNode = new ListNode(currVal % 10);
+            ListNode* it_curr = new ListNode(currVal % 10);
+            carry = currVal / 10;
 
-        if (!l2) { //there is only l1
-            currNode->next = helperAdd(l1->next, l2, currVal / 10);
+            if (!SumHead) {
+                SumHead = it_curr;
+                curr = it_curr;
+            }
+            else {
+                curr->next = it_curr;
+                curr = curr->next;
+            }
+
+            l1 = (l1) ? l1->next : nullptr;
+            l2 = (l2) ? l2->next : nullptr;
         }
-        else if (!l1) { //there is only l2
-            currNode->next = helperAdd(l1, l2->next, currVal / 10);
+        
+        if (carry) {
+            curr->next = new ListNode(carry);
         }
-        else {
-            currNode->next = helperAdd(l1->next, l2->next, currVal / 10);
-        }
-        return currNode;
+        return SumHead;
     }
 };
